8065d4f7f30895d162e162624dbb3a3d1b358569
Joe Robinson <Joe Robinson>
2016-04-24 11:30:05 -0700

    Add controllers and views for CD and Precinct

--- caucus-manager/app/controllers/congressionaldistricts_controller.rb
+++ caucus-manager/app/controllers/congressionaldistricts_controller.rb
@@ -1,2 +1,52 @@
 class CongressionaldistrictsController < ApplicationController
+    http_basic_authenticate_with name: "caucus", password: "Manager-2016", except: [:index, :show]
+    
+    def index
+        @congressionaldistricts = CongressionalDistrict.all
+    end
+    
+    def show
+        @congressionaldistrict = CongressionalDistrict.find(params[:id])
+    end
+    
+    def new
+        @congressionaldistrict = CongressionalDistrict.new
+    end
+    
+    def edit
+        @congressionaldistrict = CongressionalDistrict.find(params[:id])
+    end
+    
+    def create
+        @congressionaldistrict = CongressionalDistrict.new(congressionaldistrict_params)
+ 
+        if @congressionaldistrict.save
+            redirect_to congressionaldistrict_path(@congressionaldistrict)
+        else
+            render 'new'
+        end
+        #render plain: params[:congressionaldistricts].inspect
+    end
+    
+    def update
+        @congressionaldistrict = CongressionalDistrict.find(params[:id])
+        
+        if @congressionaldistrict.update(congressionaldistrict_params)
+            redirect_to congressionaldistrict_path(@congressionaldistrict)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @congressionaldistrict = CongressionalDistrict.find(params[:id])
+        @congressionaldistrict.destroy
+ 
+        redirect_to congressionaldistricts_path
+    end
+    
+    private
+    def congressionaldistrict_params
+        params.require(:congressionaldistrict).permit(:name)
+    end
 end
--- caucus-manager/app/controllers/precincts_controller.rb
+++ caucus-manager/app/controllers/precincts_controller.rb
@@ -1,2 +1,52 @@
 class PrecinctsController < ApplicationController
+    http_basic_authenticate_with name: "caucus", password: "Manager-2016", except: [:index, :show]
+    
+    def index
+        @precincts = Precinct.all
+    end
+    
+    def show
+        @precinct = Precinct.find(params[:id])
+    end
+    
+    def new
+        @precinct = Precinct.new
+    end
+    
+    def edit
+        @precinct = Precinct.find(params[:id])
+    end
+    
+    def create
+        @precinct = Precinct.new(precinct_params)
+ 
+        if @precinct.save
+            redirect_to precinct_path(@precinct)
+        else
+            render 'new'
+        end
+        #render plain: params[:precincts].inspect
+    end
+    
+    def update
+        @precinct = Precinct.find(params[:id])
+        
+        if @precinct.update(precinct_params)
+            redirect_to precinct_path(@precinct)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @precinct = Precinct.find(params[:id])
+        @precinct.destroy
+ 
+        redirect_to precincts_path
+    end
+    
+    private
+    def precinct_params
+        params.require(:precinct).permit(:name)
+    end
 end
