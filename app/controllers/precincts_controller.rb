f781c5c0396c4ac3fb23fc6f72f5997e58ad82e0
Joe Robinson <joroconsulting@gmail.com>
2016-04-27 23:06:12 -0700

    Update precinct model references

--- app/controllers/precincts_controller.rb
+++ app/controllers/precincts_controller.rb
@@ -47,6 +47,6 @@ class PrecinctsController < ApplicationController
     
     private
     def precinct_params
-        params.require(:precinct).permit(:name, :congressionaldistrict_id, :legislativedistrict_id)
+        params.require(:precinct).permit(:name, :congressional_district_id, :legislative_district_id)
     end
 end
--- app/models/precinct.rb
+++ app/models/precinct.rb
@@ -1,5 +1,5 @@
 class Precinct < ActiveRecord::Base
     has_many :citizens
-    belongs_to :congressional_district, class_name: "CongressionalDistrict", foreign_key: "congressionaldistrict_id"
-    belongs_to :legislative_district, class_name: "LegislativeDistrict", foreign_key: "legislativedistrict_id"
+    belongs_to :congressional_district, class_name: "CongressionalDistrict", foreign_key: "congressional_district_id"
+    belongs_to :legislative_district, class_name: "LegislativeDistrict", foreign_key: "legislative_district_id"
 end
